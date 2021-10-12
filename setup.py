7812c75582906239c2861e04549c2713936d7e6e
Benjamin Bertrand <beenje@gmail.com>
2021-10-12 21:05:35 +0200

    Add python:3.10 tests

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="gitlab sans-io async",
     packages=setuptools.find_packages(exclude=("tests", "tests.*")),
